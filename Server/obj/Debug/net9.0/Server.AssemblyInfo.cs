ba5ad3cc2f65418da0d476d7b97e57191f7dcc0a
BrandonNeblettCanCode <NeblettProtonMail2026@pm.me>
2026-08-13 13:53:43 -0500

    Completed V1 of OTP system

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d84a9fa66e49ddac92d0a071c189bd9157c19442")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f607247f13c2bb2f541a573d4966fd7c30d10caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
